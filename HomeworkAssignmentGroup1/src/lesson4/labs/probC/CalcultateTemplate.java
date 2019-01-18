e4b756a8158db4ec1242e99da4f1a72c0a3d450c
flormariavg <flormariavg@gmail.com>
2019-01-18 13:38:59 -0600

    Lab4 ProbC

--- HomeworkAssignmentGroup1/src/lesson4/labs/probC/CalcultateTemplate.java
+++ HomeworkAssignmentGroup1/src/lesson4/labs/probC/CalcultateTemplate.java
@@ -78,5 +78,28 @@ public class CalcultateTemplate {
 		
 	}
 	
+	/*
+	 * OUTPUT SOLUTION:
+	 * 
+	 * 	EmployeeID = 1  Hourly => hourlyWage = 15.0 hoursPerWeek= 20.0
+		Month/Year Report: 11/2018
+		grossPay = 1200.0 netPay = 725.9999999999999
+		EmployeeID = 2  Hourly => hourlyWage = 12.0 hoursPerWeek= 20.0
+		Month/Year Report: 11/2018
+		grossPay = 960.0 netPay = 580.8
+		EmployeeID = 3  Salaried -> salary = 2000.0
+		Month/Year Report: 11/2018
+		grossPay = 2000.0 netPay = 1210.0
+		EmployeeID = 4  Salaried -> salary = 4000.0
+		Month/Year Report: 11/2018
+		grossPay = 4000.0 netPay = 2420.0
+		EmployeeID = 5  Commissioned => commission = 0.1 baseSalary= 1500.0
+		Month/Year Report: 11/2018
+		grossPay = 1620.0 netPay = 980.0999999999999
+		EmployeeID = 6  Commissioned => commission = 0.15 baseSalary= 2000.0
+		Month/Year Report: 11/2018
+		grossPay = 2180.0 netPay = 1318.8999999999999
+	 */
+	
 }
 
