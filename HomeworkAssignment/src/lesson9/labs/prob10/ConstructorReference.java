4df18159a8d239a74fda0ff2486a1c578bfeca9e
flormariavg <flormariavg@gmail.com>
2019-02-04 19:19:16 -0600

    W2-L9-HOMEWORK

--- HomeworkAssignment/src/lesson9/labs/prob10/ConstructorReference.java
+++ HomeworkAssignment/src/lesson9/labs/prob10/ConstructorReference.java
@@ -99,7 +99,22 @@ public static void main(String args[]){
 		System.out.println(maleCandidate);
     
    }
+/*
+ * OUTPUT SOLUTION:
 
-
+	Query 1  : Print only Female canditates names
+	
+	Human [name=Jane, age=45, gender=Female]
+	****************************************************************************
+	Query 2 : Create an objecy by choosing suitable Interface to the specified constructors
+	
+	Human [name=null, age=0, gender=null]
+	Human [name=Flor, age=0, gender=null]
+	Human [name=Ann, age=25, gender=null]
+	Human [name=Jhon, age=32, gender=Male]
+	****************************************************************************
+	Query 3 : Count the male candidates whose age is more than 30
+	1
+ */
 
 }
